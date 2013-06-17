96eb3f71b3aa1452f246200cab689de21a94cef0
François <briatte@gmail.com>
2013-06-17 04:22:27 +0200

    dumping scrapers

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 ![](figure.png)
 
-Replication files for some of the figures in the paper. Work in progress.
+Replication files for some of the figures in the paper.
+
+* `google-trends.R` reproduces Figure 1 using data collected with [Google Trends][gt] in May 2013
+* `scrapers.R` reproduces Figure 2 using data collected with [scrapers][cs] for Numerama and ZDNet in May 2013
+* `voson.R` reproduces Figures 3 (shown above) and 4 using data collected with [VOSON][vs] in 2008 and 2011
+
+See also the `flegscrap` and `ggnet` repositories for related work.
+
+[cs]: /
+[gt]: /
+[vs]: http://voson.anu.edu.au/
+
+**Work in progress.**
---
+++ data/build.corpus.R
@@ -0,0 +1,54 @@
+# --------
+# packages
+# --------
+
+require(ggplot2)
+require(RCurl)
+require(reshape)
+require(stringr)
+require(XML)
+
+# ------
+# folder
+# ------
+
+setwd("data")
+
+# ---------
+# stopwords
+# ---------
+
+sw <- getURL("http://members.unine.ch/jacques.savoy/clef/frenchST.txt")
+sw <- readLines(textConnection(sw))
+
+# --------
+# keywords
+# --------
+
+keywords <- function(d) {
+  #
+  # extract keywords
+  #
+  kw = sapply(d, function(x) {
+    t <- readLines(x)[-2]
+    t <- gsub("\\s{2,}", " ", t, perl = TRUE) # trim double spaces
+    r <- "((rapport|commission|mission)*\\s*[A-ZÉ]+[a-zâäàêëéèîïôöûüç-]*\\s*(-|à |de |de la |d'|de l'|du |des )*)+"
+    k <- unique(str_trim(unlist(str_extract_all(t, r))))
+    s <- which(tolower(k) %in% c(sw))
+    k[-s]
+  })
+  #
+  # clean prefixes and suffixes
+  #
+  r = "^Culture |^UMP |^Mme |^Madame |^Me |^La |^Le |^Les |^Dans |^En |^Ce |^Lors de |^Lors de l'|^Lors de la|^Lors du |^Lors des |^Selon |^Selon le |^Selon la |^Selon les |^Un |^Comment |^Peut-être |^Mais |^Malgré |^Même |^Merci |^Rendez-nous |^Sur |^du |^Or | à$| de$| de la$| d'$| de l'$| du$| des$| à des$| -$"
+  kw <- lapply(kw, function(x) gsub(r, "", x))
+  #
+  # return keywords
+  #
+  return(kw)
+}
+
+source("build.numerama.corpus.R")
+# 2. Numerama: HADOPI
+# 3. ZDNet: DADVSI
+# 4. ZDNet: HADOPI
